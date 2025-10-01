44d67b4656ddf555d1364881adcb391d0844b2b5
Marco Klaassen <mklaasse@redhat.com>
2025-10-01 15:02:44 +0000

    configure kafka consumer

--- src/main/java/org/acme/ContractListener.java
+++ src/main/java/org/acme/ContractListener.java
@@ -1,6 +1,11 @@
 package org.acme;
 
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
 public class ContractListener {
 
     @Inject
